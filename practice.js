a08fcb14de0eb8a61f7783c30bd643052dbd175c
stian <stiankarlsen709@gmail.com>
2024-08-27 09:39:47 +0200

    avsluttende oppgave web1

---
+++ practice.js
@@ -0,0 +1,25 @@
+// const prices = [5, 20, 10, 30, 45];
+
+// const total = prices.reduc();
+
+// console.log(`${total.toFixed(2)}`) 
+
+
+// function sum(accumulator, element){
+//     return accumulator + element; 
+// }
+
+// const grades = [75, 90, 50, 65, 25];
+
+// const maximum = grades.reduce(getMax);
+// const minimum = grades.reduce(getMin);
+// console.log(maximum);
+
+// function getMax(accumulator, element){
+//     return Math.max(accumulator, element);
+// }
+
+
+// function getMin(accumulator, element){
+//     return Math.min(accumulator, element);
+// }
